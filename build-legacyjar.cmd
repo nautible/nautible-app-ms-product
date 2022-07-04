95b404683536330142a759b001fe43d257ce191f
Kashiwazaki Tetsuya <Kashiwazaki_Tetsuya@ogis-ri.co.jp>
2022-07-04 18:36:49 +0900

    nautible/issues#2 - fix review pointed out

--- build-legacyjar.cmd
+++
@@ -1,8 +0,0 @@
-cmd /c "mvn clean package -P %1 -Dquarkus.package.type=legacy-jar -DskipTests=true"
-if not %ERRORLEVEL% == 0 (
-    echo "[ERROR] ----------------------[ maven build error ]-----------------------"
-    exit 1
-)
-
-:dockerbuild
-cmd /c "docker build -t %IMAGE% -f ./src/main/docker/Dockerfile.legacy-jar ."
--- build-native.cmd
+++
@@ -1,8 +0,0 @@
-cmd /c "mvn clean package -Pnative -Dquarkus.native.container-runtime=docker -Dquarkus.container-image.push=true -Dquarkus.container-image.group=localhost:5000 -Dquarkus.container-image.name=nautible-app-ms-product -Dquarkus.container-image.tag=latest -DskipTests=true"
-if not %ERRORLEVEL% == 0 (
-    echo "[ERROR] ----------------------[ maven build error ]-----------------------"
-    exit 1
-)
-
-:dockerbuild
-cmd /c "docker build -t %IMAGE% -f ./src/main/docker/Dockerfile.native ."
